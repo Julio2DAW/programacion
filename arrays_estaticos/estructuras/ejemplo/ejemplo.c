7e0808a304f7ffad1d3826619822013c678bdc26
Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-04-15 13:19:17 +0200

    falla el método de ordenación de la burbuja

--- arrays_estaticos/estructuras/ejemplo/ejemplo.c
+++ arrays_estaticos/estructuras/ejemplo/ejemplo.c
@@ -22,7 +22,7 @@ main(){
 	
 	for (i=0; i<=2; i++) {
 		
-		printf("Nº Exp alumnos[%d].nexp: ", i);
+		printf("N� Exp alumnos[%d].nexp: ", i);
 		scanf("%d", &alumnos[i].nexp);
 		printf("Dia alumnos[%d].fecha_nac.dd: ", i);
 		scanf("%d", &alumnos[i].fecha_nac.dd);
@@ -53,7 +53,7 @@ main(){
 		}
 	}
 	
-	printf("\nOrdenar con el método switch de mayor a menor por orden de expediente\n\n");
+	printf("\nOrdenar con el m�todo switch de mayor a menor por orden de expediente\n\n");
 	
 	sw = 0;
 	while (sw==0){
@@ -62,7 +62,7 @@ main(){
 		
 		for (i=0; i<=2; i++){
 			
-			if(alumnos[i].nexp < alumnos[i+1].nexp){
+			if(alumnos[i].nexp < alumnos[i+1].nexp) {
 				
 				aux=alumnos[i];
 				alumnos[i]=alumnos[i+1];
@@ -73,7 +73,37 @@ main(){
 		}
 	}
 	
-	printf("\nVisualizar alumnos ordenados\n");
+	printf("\nVisualizar alumnos ordenados por switch\n");
+	
+	for (i=0; i<=2; i++) {
+		
+		printf("\nalumnos[%d].nexp = %d", i, alumnos[i].nexp);
+		printf("\nalumnos[%d].fecha_nac.dd = %d", i, alumnos[i].fecha_nac.dd);
+		printf("\nalumnos[%d].fecha_nac.mm = %d", i, alumnos[i].fecha_nac.mm);
+		printf("\nalumnos[%d].fecha_nac.aa = %d", i, alumnos[i].fecha_nac.aa);
+		
+		for (j=0; j<=2; j++) {
+			
+			printf("\nalumnos[%d].notas[%d] = %d", i, j, alumnos[i].notas[j]);
+		}
+	}
+	
+	printf("\nOrdenar con el m�todo de la burbuja de menor a mayor por orden de expediente\n\n");
+	
+	for (i=0; i<=2; i++) {
+		
+		for (j=0; j<=2-i; j++) {
+			
+			if (alumnos[j].nexp > alumnos[j+1].nexp) {
+				
+				aux=alumnos[j];
+				alumnos[j]=alumnos[j+1];
+				alumnos[j+1]=aux;
+			}
+		}
+	}
+	
+	printf("\nVisualizar alumnos ordenados por burbuja\n");
 	
 	for (i=0; i<=2; i++) {
 		
